136f8d3cc9b7c04896423e5ccaf604daa0cc71b1
Satish9047 <satishprajapati930@gmail.com>
2023-10-06 14:42:54 +0545

    tailwind css added to the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,29 +1,45 @@
-import {useState} from "react";
-import {Link} from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
-    const [email, setEmail]=useState("");
-    const [password, setPassword] = useState("");
-    const handleLogin = (ev)=>{
-        ev.preventDefault()
-    }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = (ev) => {
+    ev.preventDefault();
+  };
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-screen bg-[#FFF]">
-        <div className="flex flex-col justify-center items-center w-4/12 bg-[#45FFCA] rounded-lg">
-        <div>
-            Login
-        </div>
-        <form onSubmit={handleLogin} className=" flex flex-col w-10/12">
-            <label>email</label>
-            <input  value={email} onChange={(ev)=>setEmail(ev.target.value)}/>
-            <label>password</label>
-            <input value={password} onChange={(ev)=>setPassword(ev.target.value)}/>
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <div className="flex flex-col justify-center items-center  w-4/12  bg-[#FFE5E5] rounded-lg py-4 px-2">
+        <div className=" text-4xl font-sans">Login</div>
+        <form onSubmit={handleLogin} className=" flex flex-col w-10/12 px-1 py-2">
+          <label htmlFor="email">Email</label>
+          <input 
+          id="email"
+          value={email}
+          type="email"
+          className="p-1 mb-1"
+          onChange={(ev) => setEmail(ev.target.value)} 
+          />
 
-            <button type="submit">Login</button>
+          <label htmlFor="password">Password</label>
+          <input
+          id="password"
+            value={password}
+            type="password"
+            className="p-1 mb-1"
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+
+          <button className="bg-[#FFBFBF] hover:bg-[#FFE5E5] active:bg-[#A8DF8E] p-1 my-4" type="submit">Login</button>
         </form>
 
-        <label>Don't have account? <strong><Link to={"/register"}>Click Here</Link></strong></label>
-        </div>
+        <label>
+          Don't have account?{" "}
+          <strong>
+            <Link to={"/register"}>Click Here</Link>
+          </strong>
+        </label>
+      </div>
     </div>
-  )
-}
-export default Login
+  );
+};
+export default Login;
